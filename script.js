be34275fa9269053ed9e1e2401dda1f81686c2da
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-08-15 11:07:56 +0300

    updated nav

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $('.carousel').carousel({
+        interval: 7000,
+        pause: false,
+        wrap: false,
+        keyboard: false
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+});
