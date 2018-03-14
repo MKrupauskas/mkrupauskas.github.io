c5801a2d015a2d3dd4b836ddebc0d932e6e990ab
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-03-14 20:58:05 +0200

    emoji solved

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ document
 // footer date updating
 document.querySelector("#date").innerHTML = new Date().getFullYear();
 
-
 /**
  *
  * Here's a thing that will look through all the text nodes of a document, and
@@ -25,12 +24,11 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
  *
  */
 (function replaceEmojiWithImages(root) {
-
   var REGIONAL_INDICATOR_A = parseInt("1f1e6", 16),
-      REGIONAL_INDICATOR_Z = parseInt("1f1ff", 16),
-      IMAGE_HOST = "assets.github.com",
-      IMAGE_PATH = "/images/icons/emoji/unicode/",
-      IMAGE_EXT = ".png";
+    REGIONAL_INDICATOR_Z = parseInt("1f1ff", 16),
+    IMAGE_HOST = "assets.github.com",
+    IMAGE_PATH = "/images/icons/emoji/unicode/",
+    IMAGE_EXT = ".png";
 
   // String.fromCodePoint is super helpful
   if (!String.fromCodePoint) {
@@ -39,16 +37,23 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
      * (c) 2012 Steven Levithan <http://slevithan.com/>
      * MIT License
      **/
-    String.fromCodePoint = function fromCodePoint () {
-      var chars = [], point, offset, units, i;
+    String.fromCodePoint = function fromCodePoint() {
+      var chars = [],
+        point,
+        offset,
+        units,
+        i;
       for (i = 0; i < arguments.length; ++i) {
         point = arguments[i];
         offset = point - 0x10000;
-        units = point > 0xFFFF ? [0xD800 + (offset >> 10), 0xDC00 + (offset & 0x3FF)] : [point];
+        units =
+          point > 0xffff
+            ? [0xd800 + (offset >> 10), 0xdc00 + (offset & 0x3ff)]
+            : [point];
         chars.push(String.fromCharCode.apply(null, units));
       }
       return chars.join("");
-    }
+    };
   }
 
   /**
@@ -60,20 +65,22 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
   function getLegitTextNodes(element) {
     if (!document.createTreeWalker) return [];
 
-    var blacklist = ['SCRIPT', 'OPTION', 'TEXTAREA'],
-        textNodes = [],
-        walker = document.createTreeWalker(
-            element,
-            NodeFilter.SHOW_TEXT,
-            function excludeBlacklistedNodes(node) {
-              if (blacklist.indexOf(node.parentElement.nodeName.toUpperCase()) >= 0) return NodeFilter.FILTER_REJECT;
-              if (String.prototype.trim && !node.nodeValue.trim().length) return NodeFilter.FILTER_SKIP;
-              return NodeFilter.FILTER_ACCEPT;
-            },
-            false
-        );
+    var blacklist = ["SCRIPT", "OPTION", "TEXTAREA"],
+      textNodes = [],
+      walker = document.createTreeWalker(
+        element,
+        NodeFilter.SHOW_TEXT,
+        function excludeBlacklistedNodes(node) {
+          if (blacklist.indexOf(node.parentElement.nodeName.toUpperCase()) >= 0)
+            return NodeFilter.FILTER_REJECT;
+          if (String.prototype.trim && !node.nodeValue.trim().length)
+            return NodeFilter.FILTER_SKIP;
+          return NodeFilter.FILTER_ACCEPT;
+        },
+        false
+      );
 
-    while(walker.nextNode()) textNodes.push(walker.currentNode);
+    while (walker.nextNode()) textNodes.push(walker.currentNode);
 
     return textNodes;
   }
@@ -83,10 +90,10 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
    */
   function doesSupportEmoji() {
     var context, smiley;
-    if (!document.createElement('canvas').getContext) return;
-    context = document.createElement('canvas').getContext('2d');
-    if (typeof context.fillText != 'function') return;
-    smile = String.fromCodePoint(0x1F604); // :smile: String.fromCharCode(55357) + String.fromCharCode(56835)
+    if (!document.createElement("canvas").getContext) return;
+    context = document.createElement("canvas").getContext("2d");
+    if (typeof context.fillText != "function") return;
+    smile = String.fromCodePoint(0x1f604); // :smile: String.fromCharCode(55357) + String.fromCharCode(56835)
 
     context.textBaseline = "top";
     context.font = "32px Arial";
@@ -99,16 +106,16 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
    * return a Unicode codepoint.
    */
   function surrogatePairToCodepoint(lead, trail) {
-    return (lead - 0xD800) * 0x400 + (trail - 0xDC00) + 0x10000;
+    return (lead - 0xd800) * 0x400 + (trail - 0xdc00) + 0x10000;
   }
 
   /**
    * Get an Image element for an emoji codepoint (in hex).
    */
   function getImageForCodepoint(hex) {
-    var img = document.createElement('IMG');
+    var img = document.createElement("IMG");
 
-    img.style.width = "1.4em";
+    img.style.width = "1.2em";
     img.style.verticalAlign = "top";
     img.src = "//" + IMAGE_HOST + IMAGE_PATH + hex + IMAGE_EXT;
 
@@ -119,13 +126,13 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
    * Convert an HTML string into a DocumentFragment, for insertion into the dom.
    */
   function fragmentForString(htmlString) {
-    var tmpDoc = document.createElement('DIV'),
-        fragment = document.createDocumentFragment(),
-        childNode;
+    var tmpDoc = document.createElement("DIV"),
+      fragment = document.createDocumentFragment(),
+      childNode;
 
     tmpDoc.innerHTML = htmlString;
 
-    while(childNode = tmpDoc.firstChild) {
+    while ((childNode = tmpDoc.firstChild)) {
       fragment.appendChild(childNode);
     }
     return fragment;
@@ -137,15 +144,18 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
   function emojiReplace(nodes) {
     var PATTERN = /([\ud800-\udbff])([\udc00-\udfff])/g;
 
-    nodes.forEach(function (node) {
+    nodes.forEach(function(node) {
       var replacement,
-          value = node.nodeValue,
-          matches = value.match(PATTERN);
+        value = node.nodeValue,
+        matches = value.match(PATTERN);
 
       if (matches) {
-        replacement = value.replace(PATTERN, function (match, p1, p2) {
-          var codepoint = surrogatePairToCodepoint(p1.charCodeAt(0), p2.charCodeAt(0)),
-              img = getImageForCodepoint(codepoint.toString(16));
+        replacement = value.replace(PATTERN, function(match, p1, p2) {
+          var codepoint = surrogatePairToCodepoint(
+              p1.charCodeAt(0),
+              p2.charCodeAt(0)
+            ),
+            img = getImageForCodepoint(codepoint.toString(16));
           return img.outerHTML;
         });
 
@@ -158,5 +168,4 @@ document.querySelector("#date").innerHTML = new Date().getFullYear();
   if (!doesSupportEmoji()) {
     emojiReplace(getLegitTextNodes(document.body));
   }
-
-}(this));
+})(this);
