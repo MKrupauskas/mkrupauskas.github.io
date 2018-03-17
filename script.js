bb2ac75f036358701a9dc918a64d4de3484bfcb4
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-03-17 19:54:59 +0200

    add progressively enhanced smooth scrolling

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document
 
 // footer date updating
 document.querySelector("#date").innerHTML = new Date().getFullYear();
+
+// smooth scrolling
+document.querySelectorAll(".navigation__item").forEach(item => {
+  item.addEventListener("click", function() {
+    event.preventDefault();
+    document
+      .querySelector(`.${item.childNodes[1].innerHTML.toLowerCase()}`)
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      });
+  });
+});
